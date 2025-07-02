0f4cbea3baba33724754af21dafee79fe60e4998
detector_de_Rostros <michaelrxx55@gmail.com>
2025-07-02 14:07:21 -0500

    Fix: configurar proxies en constructor para evitar error en tiempo de carga

--- app/Http/Middleware/TrustProxiesCustom.php
+++ app/Http/Middleware/TrustProxiesCustom.php
@@ -2,22 +2,22 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Support\Facades\App;
 
 class TrustProxiesCustom extends Middleware
 {
-    /**
-     * Confiar en todos los proxies (como Render).
-     *
-     * @var array|string|null
-     */
-    protected $proxies = '*';
-
     /**
      * Usar todos los encabezados relacionados con proxies.
      *
      * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    public function __construct()
+    {
+        // Solo confiar en proxies si estamos en producción
+        $this->proxies = App::environment('production') ? '*' : null;
+    }
 }
