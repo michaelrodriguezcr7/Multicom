ef322ad4d2ae0980117db3b37068ac2d4a5454e1
detector_de_Rostros <michaelrxx55@gmail.com>
2025-07-02 14:13:03 -0500

    Fix: configurar proxies para la ip

--- app/Http/Middleware/TrustProxiesCustom.php
+++ app/Http/Middleware/TrustProxiesCustom.php
@@ -2,19 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Support\Facades\App;
 
 class TrustProxiesCustom extends Middleware
 {
-    /**
-     * Usar todos los encabezados relacionados con proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
-
     public function __construct()
     {
         // Solo confiar en proxies si estamos en producción
